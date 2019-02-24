5da969c974e87fc77e1ef6f11c1bc409d86d37bc
William Okafor <wilpat456@gmail.com>
2019-02-24 19:25:06 +0100

    Upload to github

--- sec.js
+++ sec.js
@@ -1,6 +1,6 @@
-const mnemonicTxt = "mansion vocal brother anxiety empower oak noise prosper select rival intact rich";
-const infura_key = '9ce3fce3a4c34accaa192f68222eff48';
-const kaleido_key = 'https://e0n06zi2jf:o7pe38R9iUyf3Nh5HuYTtV1e_hzQG_DlYckeWA-ICd0@e0ssu47hxk-e0l91isw19-rpc.eu-central-1.kaleido.io';
+const mnemonicTxt = "<your-ropsten-wallet-mnemonic>";
+const infura_key = '<infura-secret>';
+const kaleido_key = 'https://<kaleido-username>:<kaleido-password>@<kaleido-rpc>';
 
 module.exports ={
 	mnemonicTxt,
